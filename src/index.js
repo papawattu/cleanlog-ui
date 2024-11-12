cddceeafafc63bae5512e38e3fbf3e7a86793760
Jamie Nuttall <jamie@wattu.com>
2024-11-12 07:44:19 +0000

    Cleaned up and working

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ const app = express()
 // express router
 const router = express.Router()
 
-app.engine('html', es6Renderer);
-app.set('views', 'views');
-app.set('view engine', 'html');
+app.engine('html', es6Renderer)
+app.set('views', 'views')
+app.set('view engine', 'html')
 
 app.use(cors())
 app.use(cookieParser())
@@ -25,349 +25,347 @@ app.use(express.static('public'))
 app.use(router)
 
 router.get('/', (req, res) => {
-    res.cookie('token', 'mytoken')
-    res.render('index')
+  res.cookie('token', 'mytoken')
+  res.render('index')
 })
 
+function callApi({ url, method, token, body, baseUrl }) {
+  return fetch(baseUrl + url, {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + token,
+    },
+    body: body ? JSON.stringify(body) : undefined,
+  })
+}
+
+function callWorkLogApi({ url, method, token, body }) {
+  return callApi({
+    url,
+    method,
+    token,
+    body,
+    baseUrl: process.env.WORK_LOG_URI || 'http://localhost:3001',
+  })
+}
+
+function callTaskApi({ url, method, token, body }) {
+  return callApi({
+    url,
+    method,
+    token,
+    body,
+    baseUrl: process.env.TASK_URI || 'http://localhost:3002',
+  })
+}
 router.get('/worklog', async (req, res) => {
+  const workList = await callWorkLogApi({
+    url: '/api/worklog/',
+    method: 'GET',
+    token: req.cookies['token'],
+  })
 
-    const workList = await fetch('http://localhost:3001/api/worklog/', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + req.cookies['token']
-        }
-    }).catch(err => {
-        console.error(err)
-        res.send('Error').status(500)
-        return
-    })
+  if (!workList) {
+    res.send('Error').status(500)
+    return
+  }
+  if (workList.status !== 200) {
+    res.send(`Error ${workList.status}`).status(500)
+    return
+  }
+  const work = await workList.json()
 
-    if(!workList) {
-        res.send('Error').status(500)
-        return
-    }
-    if(workList.status !== 200) {
-        res.send(`Error ${workList.status}`).status(500)
-        return
-    }
-    const work = await workList.json()
-
-    res.render('worklog',{ locals: { workList: work.worklogs ? work.worklogs : [] }})
+  res.render('worklog', {
+    locals: { workList: work.worklogs ? work.worklogs : [] },
+  })
 })
 router.get('/addwork', (req, res) => {
-    res.render('addwork', { locals: { work : { description: '', date: '', hours: '' } }})
+  res.render('addwork', {
+    locals: { work: { description: '', date: '', hours: '' } },
+  })
 })
 router.get('/editwork/:id', async (req, res) => {
-    const id = req.params.id
-    const workList = await fetch(`http://localhost:3001/api/worklog/${id}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + req.cookies['token']
-        }
-    })
+  const id = req.params.id
 
-    const work = await workList.json()
+  const workList = await callWorkLogApi({
+    url: `/api/worklog/${id}`,
+    method: 'GET',
+    token: req.cookies['token'],
+  })
 
-    res.render('addwork', { locals: { work }})
+  const work = await workList.json()
+
+  res.render('addwork', { locals: { work } })
 })
 router.post('/addwork', async (req, res) => {
-    const { description, date, hours } = req.body
+  const { description, date, hours } = req.body
 
-    const resp = await fetch('http://localhost:3001/api/worklog', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + req.cookies['token']
-        },
-        body: JSON.stringify({ description, date, hours })
-    }).catch(err => {
+  const resp = await callWorkLogApi({
+    url: '/api/worklog',
+    method: 'POST',
+    token: req.cookies['token'],
+    body: { description, date, hours },
+  }).catch((err) => {
+    console.error(err)
+    res.send('Error').status(500)
+    return
+  })
+
+  if (!resp) {
+    return
+  }
+
+  if (resp.status !== 201) {
+    res.send('Error ' + resp.statusText).status(resp.status)
+    return
+  }
+
+  if (!resp.headers.has('Location')) {
+    res.send('Error location expected').status(500)
+    return
+  }
+
+  const location = resp.headers.get('Location')
+
+  let r,
+    retries = 0
+  do {
+    r = callWorkLogApi({
+      url: location,
+      method: 'GET',
+      token: req.cookies['token'],
+    })
+      .then((res) => {
+        r = res
+      })
+      .catch((err) => {
         console.error(err)
         res.send('Error').status(500)
         return
-    })
+      })
 
-    if(!resp) {
-        return
+    if (r.status !== 200) {
+      retries++
+      await new Promise((resolve) => setTimeout(resolve, 200))
     }
+  } while (r.status !== 200 && retries < 10)
 
-    if(resp.status !== 201) {
-        res.send('Error ' + resp.statusText).status(resp.status)
-        return
-    }
-    
-    if(!resp.headers.has('Location')) {
-        res.send('Error location expected').status(500)
-        return
-    } 
+  if (r.status !== 200) {
+    res.send('Timeout').status(500)
+    return
+  }
 
-    const location = resp.headers.get('Location')
+  const workList = await callWorkLogApi({
+    url: location,
+    method: 'GET',
+    token: req.cookies['token'],
+  })
+  const work = await workList.json()
 
-    let r, retries = 0
-    do {
-        r = await fetch(`http://localhost:3001${location}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + req.cookies['token']
-            }
-        }).catch(err => {
-            console.error(err)
-            res.send('Error').status(500)
-            return
-        })
+  const taskList = await getTaskList(work.taskIds, req.cookies['token'])
 
-        if(r.status !== 200) {
-            retries++
-            await new Promise(resolve => setTimeout(resolve, 200))
-        }
-    } while (r.status !== 200 && retries < 10)
-
-    if(r.status !== 200) {
-        res.send('Timeout').status(500)
-        return
-    }
-
-    const workList = await fetch('http://localhost:3001/api/worklog/', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + req.cookies['token'],
-        }
-    })
-    
-    const { worklogs } = await workList.json()
-        .catch(err => {
-            console.error(err)
-            res.send('Error cannot unmarshal json').status(500)
-            return
-        })  
-    
-
-    res.render('worklog',{ locals: { workList: worklogs }})
+  res.render('tasks', { locals: { work, taskList } })
 })
 router.post('/editwork/:id', async (req, res) => {
-    const id = req.params.id
-    const { description, date, hours } = req.body
-    
-    const resp = await fetch('http://localhost:3001/api/worklog/' + id, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + req.cookies['token']
-        },
-        body: JSON.stringify({ description, date, hours })
-    }).catch(err => {
-    
-        res.send('Error ' + err).status(500)
-        return
-    })
+  const id = req.params.id
+  const { description, date, hours } = req.body
 
-    if(!resp) {
-        return
-    }
+  const resp = await callWorkLogApi({
+    url: `/api/worklog/${id}`,
+    method: 'PUT',
+    token: req.cookies['token'],
+    body: { description, date, hours },
+  }).catch((err) => {
+    console.error(err)
+    res.send('Error').status(500)
+    return
+  })
 
-    if(resp.status !== 200) {
-        res.send('Error').status(500)
-        return
-    }
-    const json = await resp.json().catch(err => {
-    
-        res.send('Error').status(500)
-        return
-    
-    })
-    
-    const workList = await fetch('http://localhost:3001/api/worklog/', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + req.cookies['token'],
-        }
-    })
+  if (!resp) {
+    return
+  }
 
-    const { worklogs } = await workList.json()
-    
-    
-    res.render('worklog',{ locals: { workList: worklogs }})
+  if (resp.status !== 200) {
+    res.send('Error').status(500)
+    return
+  }
+  const json = await resp.json().catch((err) => {
+    res.send('Error').status(500)
+    return
+  })
+
+  const workList = await callWorkLogApi({
+    url: '/api/worklog/',
+    method: 'GET',
+    token: req.cookies['token'],
+  })
+
+  const { worklogs } = await workList.json()
+
+  res.render('worklog', { locals: { workList: worklogs } })
 })
 router.get('/editwork/:id', async (req, res) => {
-    const id = req.params.id
-    const workList = await fetch(`http://localhost:3001/api/worklog/${id}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + req.cookies['token']
-        }
-    })
-    const { worklogs } = await workList.json()
-    worklogs.id = id
-    res.render('editwork',{ locals: { work: worklogs }})
+  const id = req.params.id
+
+  const workList = await callTaskApi({
+    url: `/api/worklog/${id}`,
+    method: 'GET',
+    token: req.cookies['token'],
+  })
+
+  const { worklogs } = await workList.json()
+  worklogs.id = id
+  res.render('editwork', { locals: { work: worklogs } })
 })
 
 router.get('/deletework/:id', async (req, res) => {
-    const id = req.params.id
-    const response = await fetch(`http://localhost:3001/api/worklog/${id}`, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + req.cookies['token']
-        }
-    })
-    
-    if(response.status !== 204) {
-        res.send('Error').status(500)
-        return
-    } 
+  const id = req.params.id
+  const response = await callWorkLogApi({
+    url: `/api/worklog/${id}`,
+    method: 'DELETE',
+    token: req.cookies['token'],
+  })
 
-    const workList = await fetch('http://localhost:3001/api/worklog/', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + req.cookies['token'],
-        }
-    })
-    const { worklogs } = await workList.json()
-    
-    res.render('worklog',{ locals: { workList: worklogs }})
-   
-})  
+  if (response.status !== 204) {
+    res.send('Error').status(500)
+    return
+  }
 
-app.get('/tasks/:id', async (req, res) => {
-    const id = req.params.id
-    const workList = await fetch(`http://localhost:3001/api/worklog/${id}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + req.cookies['token']
-        }
-    })
-    const work = await workList.json()
+  const workList = await callWorkLogApi({
+    url: '/api/worklog/',
+    method: 'GET',
+    token: req.cookies['token'],
+  })
 
-    const taskList = await Promise.all(work.taskIds.map(async taskId => {
-        const task = await fetch(`http://localhost:3002/api/task/${taskId}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + req.cookies['token']
-            }
-        })
-        return await task.json()
-    }))
-    
-    res.render('tasks',{ locals: { work, taskList }})
-})   
+  const { worklogs } = await workList.json()
+
+  res.render('worklog', { locals: { workList: worklogs } })
+})
+
+app.get('/worklog/:id', async (req, res) => {
+  const id = req.params.id
+  const workList = await callWorkLogApi({
+    url: `/api/worklog/${id}`,
+    method: 'GET',
+    token: req.cookies['token'],
+  })
+
+  if (workList.status !== 200) {
+    res.redirect('/worklog')
+    return
+  }
+  const work = await workList.json()
+
+  const taskList = await getTaskList(work.taskIds, req.cookies['token'])
+
+  res.render('tasks', { locals: { work, taskList } })
+})
 
 app.get('/addtask/:id', async (req, res) => {
-    const id = req.params.id
-    const workList = await fetch(`http://localhost:3001/api/worklog/${id}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + req.cookies['token']
-        }
-    })
-    const work = await workList.json()
+  const id = req.params.id
 
-    res.render('addtask',{ locals: { work }})
+  const workList = await callWorkLogApi({
+    url: `/api/worklog/${id}`,
+    method: 'GET',
+    token: req.cookies['token'],
+  })
+
+  const work = await workList.json()
+
+  res.render('addtask', { locals: { work } })
 })
 
 app.post('/addtask/:id', async (req, res) => {
-    const id = req.params.id
-    const { description, date, hours } = req.body
-    
-    const resp = await fetch('http://localhost:3002/api/task', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + req.cookies['token']
-        },
-        body: JSON.stringify({ description, date, hours })
-    }).catch(err => {
-    
-        res.send('Error ' + err).status(500)
-        return
+  const id = req.params.id
+  const { description, date, hours } = req.body
+
+  const resp = await callTaskApi({
+    url: '/api/task',
+    method: 'POST',
+    token: req.cookies['token'],
+    body: { description, date, hours },
+  }).catch((err) => {
+    console.error(err)
+    res.send('Error').status(500)
+    return
+  })
+
+  if (!resp) {
+    return
+  }
+
+  if (resp.status !== 201) {
+    res.send('Error').status(500)
+    return
+  }
+
+  if (!resp.headers.has('Location')) {
+    res.send('Error location expected').status(500)
+    return
+  }
+
+  const location = resp.headers.get('Location')
+
+  let r,
+    retries = 0
+
+  do {
+    r = await callTaskApi({
+      url: location,
+      method: 'GET',
+      token: req.cookies['token'],
     })
+  } while (r.status !== 200 && retries < 10)
 
-    if(!resp) {
-        return
-    }
+  if (r.status !== 200) {
+    res.send('Timeout').status(500)
+    return
+  }
+  const json = await r.json()
 
-    if(resp.status !== 201) {
-        res.send('Error').status(500)
-        return
-    }
-    
-    if(!resp.headers.has('Location')) {
-        res.send('Error location expected').status(500)
-        return
-    }
+  const r2 = await callWorkLogApi({
+    url: `/api/worklog/${id}`,
+    method: 'PATCH',
+    token: req.cookies['token'],
+    body: { taskIds: [json.taskId] },
+  })
 
-    const location = resp.headers.get('Location')
-    console.log(`Location ${location}`)
-    let r, retries = 0
+  if (r2.status !== 204) {
+    res.send('Error ' + r2.status).status(500)
+    return
+  }
 
-    do {    
-        r = await fetch(`http://localhost:3002${location}`, {
-          method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + req.cookies['token'],
-            }
-        })
+  const workReq = await callWorkLogApi({
+    url: `/api/worklog/${id}`,
+    method: 'GET',
+    token: req.cookies['token'],
+  })
 
-    } while (r.status !== 200 && retries < 10)
+  const work = await workReq.json()
 
-    if(r.status !== 200) {
-        res.send('Timeout').status(500)
-        return
-    }
-    const json = await r.json()
+  const taskList = await getTaskList(work.taskIds, req.cookies['token'])
 
-    const r2 = await fetch(`http://localhost:3001/api/worklog/${id}`, {
-        method: 'PATCH',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + req.cookies['token']
-        },
-        body: JSON.stringify({ taskIds: [json.taskId] })
-    })
-        
-    
-    if(r2.status !== 204) {
-        res.send('Error ' + r2.status).status(500)
-        return
-    }
-    
-    const workReq = await fetch(`http://localhost:3001/api/worklog/${id}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + req.cookies['token']
-        }
-    })
-    const work = await workReq.json()
-
-    const taskList = await Promise.all(work.taskIds.map(async taskId => {
-        const task = await fetch(`http://localhost:3002/api/task/${taskId}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + req.cookies['token']
-            }
-        })
-        if(task.status !== 200) {
-            console.error('Error task status :' + task.status)
-            return undefined
-        }
-        return await task.json()
-    }))
- 
-    res.render('tasks',{ locals: { work, taskList }})
+  res.render('tasks', { locals: { work, taskList } })
 })
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+  console.log('Server is running on port 3000')
 })
 
+const getTaskList = async (taskIds, token) =>
+  await Promise.all(
+    taskIds.map(async (taskId) => {
+      const task = await callTaskApi({
+        url: `/api/task/${taskId}`,
+        method: 'GET',
+        token,
+      })
+
+      if (task.status !== 200) {
+        //console.error('Error task status :' + task.status)
+        return []
+      }
+      return await task.json()
+    })
+  )
--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer')
+import puppeteer from 'puppeteer'
 
 describe('CleanLog UI Tests', () => {
   let browser
@@ -7,7 +7,7 @@ describe('CleanLog UI Tests', () => {
   beforeAll(async () => {
     browser = await puppeteer.launch({
       headless: false,
-      slowMo: 80,
+      //slowMo: 200,
       args: ['--start-maximized'],
     })
     page = await browser.newPage()
@@ -23,5 +23,61 @@ describe('CleanLog UI Tests', () => {
     expect(title).toBe('CleanLog')
   })
 
-  // Add more tests here
+  test('should display have link to work logs', async () => {
+    await page.waitForSelector('a#worklog')
+    const text = await page.evaluate(
+      () => document.querySelector('a#worklog').innerText
+    )
+    expect(text).toBe('Work Log')
+  })
+  test('should go to work log page', async () => {
+    await page.click('a#worklog')
+
+    const title = await page.title()
+
+    expect(title).toBe('Work View')
+  })
+
+  test('should display work log table', async () => {
+    await page.waitForSelector('table#worklist')
+    const el = await page.evaluate(
+      () => document.querySelector('table#worklist').innerHTML
+    )
+    console.log(el)
+    //expect(text).toBe('Work Log')
+  })
+  test('should display add worklog', async () => {
+    await page.waitForSelector('a#addworklog')
+    await page.click('a#addworklog')
+    await page.waitForSelector('form')
+    await page.waitForSelector('input#description')
+    const description = await page.evaluate(
+      () => document.querySelector('input[name="description"]').value
+    )
+    expect(description).toBe('')
+  })
+  test('should add worklog', async () => {
+    await page.type('input[name="description"]', 'Test123')
+    await page.type('input[name="date"]', '10/11/2021')
+
+    await page.click('input[type="submit"]')
+    await page.waitForSelector('table#worklist')
+
+    const text = await page.evaluate(() =>
+      Array.from(document.querySelectorAll('td a')).find(
+        (el) => el.innerText === 'Test123'
+      )
+    )
+
+    expect(text).toBeTruthy()
+  })
+  test('should select worklog', async () => {
+    const text = await page.evaluate(() =>
+      Array.from(document.querySelectorAll('td a')).find(
+        (el) => el.innerText === 'Test123'
+      )
+    )
+
+    expect(text).toBeTruthy()
+  })
 })
